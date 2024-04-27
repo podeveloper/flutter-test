5c3f965559a2ccafd0d797256d74181d2e59b9dd
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2024-04-27 17:55:33 +0300

    Dynamic Colors For Gradient Container Widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,17 +1,18 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:test_drive/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer({super.key, required this.colors});
+
+  final List<Color> colors;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Colors.pink,
-          Colors.green,
-        ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(colors: colors, begin: Alignment.topLeft, end: Alignment.bottomRight),
       ),
       child: const Center(
         child: StyledText('Hello World'),
